07878f8f99f3ef7f34badf99bdc23b5770c6898e
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-21 15:32:35 +0200

    refactor: remove navigation-config, navigation features for start page

---
+++ src/app/start/shared/features.ts
@@ -0,0 +1,38 @@
+  export const features = [
+    {
+      title: 'Simple coded',
+      description: `It has been written as simple as possible using angular framework with awesome typescript.`
+    },
+    {
+      title: 'JSON Object',
+      description: `Create html forms with the simple structure of json object.`
+    },
+    {
+      title: 'Angular Material',
+      description: `Default preconfigured Angular Material 2 form elements.`
+    },
+    {
+      title: 'Autocomplete',
+      description: `The autocomplete is a normal text input enhanced by a panel of suggested options. `
+    },
+    {
+      title: 'Checkbox',
+      description: `Provides the same functionality as a native <input type="checkbox">`
+    },
+    {
+      title: 'Input',
+      description: `Is a wrapper for native input and textarea elements.`
+    },
+    {
+      title: 'Datepicker',
+      description: `The datepicker allows users to enter a date either through text input, or by choosing a date from the calendar.`
+    },
+    {
+      title: 'Select',
+      description: `Is a form control for selecting a value from a set of options, similar to the native <select> element.`
+    },
+    {
+      title: 'Slider',
+      description: `allows for the selection of a value from a range via mouse, touch, or keyboard, similar to <input type="range">.`
+    }
+  ];
