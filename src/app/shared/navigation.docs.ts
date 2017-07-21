879963b63810930fd21e2a33eb6a39d30d93b3a8
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-21 15:33:58 +0200

    refactor: move to shared folder

---
+++ src/app/shared/navigation.docs.ts
@@ -0,0 +1,92 @@
+
+export const docsNavigation = [
+  {
+    header: 'Element Component',
+    items: [
+      {
+        icon: '',
+        routerLink: '/docs/element/autocomplete',
+        name: 'Autocomplete',
+        description: `The autocomplete is a normal text input enhanced by a panel of suggested options.`,
+        disabled: true
+      },
+      {
+        icon: '',
+        routerLink: '/docs/element/checkbox',
+        name: 'Checkbox',
+        description: `Provides the same functionality as a native <input type="checkbox">`,
+        disabled: true
+      },
+      {
+        icon: '',
+        routerLink: '/docs/element/datepicker',
+        name: 'Datepicker',
+        description: `The datepicker allows users to enter a date either through text input, or by choosing a date from the calendar.`,
+        disabled: true
+      },
+      {
+        icon: '',
+        image: `/assets/input.png`,
+        routerLink: '/docs/element/input',
+        name: 'Input',
+        description: `Wrapper for native input and textarea elements. `
+      },
+      {
+        icon: '',
+        routerLink: '/docs/element/radio-button',
+        name: 'Radio button',
+        description: `Provides the same functionality as a native <input type="radio">.`,
+        disabled: true
+      },
+      {
+        icon: '',
+        image: `/assets/select.png`,
+        routerLink: '/docs/element/select',
+        name: 'Select',
+        description: `Form control for selecting a value from a set of options, similar to the native <select> element.`
+      },
+      {
+        icon: '',
+        routerLink: '/docs/element/slider',
+        name: 'Slider',
+        description: `allows for the selection of a value from a range via mouse, touch, or keyboard, similar to <input type="range">.`,
+        disabled: true
+      },
+      {
+        icon: '',
+        routerLink: '/docs/element/slide-toggle',
+        name: 'Slide toggle',
+        description: `Is an on/off control that can be toggled via clicking or dragging.`,
+        disabled: true
+      }
+    ]
+  },
+  {
+    header: 'Native Script',
+    items: [
+      {
+        icon: '',
+        routerLink: '/docs/nativescript',
+        name: 'TextField',
+        disabled: true
+      }
+    ]
+  },
+  {
+    header: 'Other',
+    items: [
+      {
+        icon: '',
+        routerLink: '/docs/interface',
+        name: 'Interface',
+        disabled: true
+      },
+      {
+        icon: '',
+        routerLink: '/docs/type',
+        name: 'Type',
+        disabled: true
+      }
+    ]
+  }
+];
